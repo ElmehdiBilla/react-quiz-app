7195094fc3e520eea65b0b5620074408d9da7a8b
elmehdibilla <billamehdi13@gmail.com>
2024-10-30 15:11:49 +0100

    add progress bar and  next button

--- src/components/quiz.jsx
+++ src/components/quiz.jsx
@@ -1,12 +1,12 @@
-import { useRef, useState } from "react";
+import React , {useState} from "react";
 
 function Quiz (props) {
-    // const optionRef=useRef(null);
 
     const question=props.question
     const questionAnswer=question.answer
     const questionIndex=props.index
     const [selectedOption,setSelectedOption]=useState(null)
+    const progress=props.progress+'%'
 
     const selectAnswer = (index) =>{
         setSelectedOption(index)
@@ -20,17 +20,24 @@ function Quiz (props) {
 
     return (
 
-        <div className="quiz-card">
-            <h3>{question.question}</h3>
-            <ul>
-                {
-                    question.options.map((option,index) => {
-                        return (
-                            <li onClick={()=>selectAnswer(index)} className={`option ${selectedOption===index?"selected-option":""}`} key={index}>{option}</li>
-                        )
-                    })
-                }
-            </ul>
+        <div className="quiz-container">
+            <div className='progress-container'><div className='progress' style={{width:progress}}></div></div>
+            <div className="question-number">Question: {`${questionIndex+1}/${props.quizLength}`}</div>
+            <div className="quiz-card">
+                <h3>{question.question}</h3>
+                <ul>
+                    {
+                        question.options.map((option,index) => {
+                            return (
+                                <li onClick={()=>selectAnswer(index)} className={`option ${selectedOption===index?"selected-option":""}`} key={index}>{option}</li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
+            <div className="btn-container">
+                <button onClick={props.getNextQuestion} className={`btn btn-primary ${selectedOption === null ?'hidden':''}`}>{props.buttonText}</button>
+            </div>
         </div>
 
     )
